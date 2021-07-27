7559ad8424f2766faab541cbe5eaf3a711a9d2f0
Dylan Awalt-Conley <web@dylan.ac>
2021-07-27 02:42:22 -0400

    copied parallax project in for testing

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,7 @@ const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
 
 const sassCompile = () => gulp.src('eleventy/css/**/*.scss')
-    .pipe(sass({ quietDeps: true }).on('error', sass.logError))
+    .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('eleventy/_site/css'));
 
 const sassWatch = () => gulp.watch('eleventy/css/**/*.scss', sassCompile);
