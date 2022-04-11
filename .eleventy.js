590d772fc6af9fb501b91d3b016a4ccb6664fb46
Dylan Awalt-Conley <web@dylan.ac>
2022-04-11 15:52:17 -0400

    updated to latest 11ty and sass, eliminate gulp from development testing

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,15 @@
+const fs = require('fs');
+const sass = require('sass');
+
 module.exports = eleventyConfig => {
     eleventyConfig.addPassthroughCopy('eleventy/images');
     eleventyConfig.addWatchTarget('eleventy/css');
+    eleventyConfig.on('eleventy.after', () => {
+        const { css } = sass.compile('eleventy/css/main.scss', {
+            loadPaths: ['node_modules'],
+        });
+        return fs.promises.writeFile('eleventy/_site/css/main.css', css);
+    });
 
     return {
         dir: {
