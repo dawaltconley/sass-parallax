73fd48319c330c041469c38caa47874e434f97d2
Dylan Awalt-Conley <web@dylan.ac>
2022-04-11 16:21:43 -0400

    added separate page for testing css properties

--- .eleventy.js
+++ .eleventy.js
@@ -1,14 +1,19 @@
 const fs = require('fs');
 const sass = require('sass');
+const sassOpts = {
+    loadPaths: [ 'node_modules', '.' ],
+};
 
 module.exports = eleventyConfig => {
     eleventyConfig.addPassthroughCopy('eleventy/images');
     eleventyConfig.addWatchTarget('eleventy/css');
     eleventyConfig.on('eleventy.after', () => {
-        const { css } = sass.compile('eleventy/css/main.scss', {
-            loadPaths: ['node_modules'],
-        });
-        return fs.promises.writeFile('eleventy/_site/css/main.css', css);
+        const { css:main } = sass.compile('eleventy/css/main.scss', sassOpts);
+        const { css:cssProps } = sass.compile('eleventy/css/css-properties.scss', sassOpts);
+        return Promise.all([
+            fs.promises.writeFile('eleventy/_site/css/main.css', main),
+            fs.promises.writeFile('eleventy/_site/css/css-properties.css', cssProps),
+        ]);
     });
 
     return {
